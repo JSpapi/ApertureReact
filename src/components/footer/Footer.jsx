f1d253a83445152c0807c11fc2dae1b7e2b18211
JSpapi <104095661+JSpapi@users.noreply.github.com>
2023-02-09 03:54:49 +0500

    finished all footer components

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import footer from "./Footer.module.scss";
 import { FooterContacts } from "../footerContacts/FooterContacts";
+import { FaTelegram, FaInstagram, FaFacebookF } from "react-icons/fa";
 export const Footer = ({ logo }) => {
   return (
     <footer className={footer.root}>
@@ -18,6 +19,27 @@ export const Footer = ({ logo }) => {
           </div>
           <div className={footer.subscribe_left}>
             {/* SHOULD ASK THIS ELEMENT IN  FIGMA AS, IT'S NOT CORRECT*/}
+            <p className="content-subtitle">
+              SHOULD ASK THIS ELEMENT IN FIGMA AS, IT'S NOT CORRECT!
+            </p>
+          </div>
+        </div>
+
+        <div className={footer.bottom}>
+          <p className={[footer.copyright, "text-regular"].join(" ")}>
+            &copy;The Website was built by Axror Kurban using ReactJS{" "}
+            {new Date().getFullYear()}
+          </p>
+          <div className={footer.social}>
+            <a href="https://t.me/AKcoder7L" style={{ color: "var(--gray)" }}>
+              <FaTelegram size={24} />
+            </a>
+            <a href="https://t.me/AKcoder7L" style={{ color: "var(--gray)" }}>
+              <FaInstagram size={24} />
+            </a>
+            <a href="https://t.me/AKcoder7L" style={{ color: "var(--gray)" }}>
+              <FaFacebookF size={24} style={{ color: "var(--gray" }} />
+            </a>
           </div>
         </div>
       </div>
